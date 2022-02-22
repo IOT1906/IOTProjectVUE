33a435834fc40ffdf4daa8bde35e9e5f47b85b71
吕龙江 <@LAPTOP-L8OM0P7M>
2022-02-22 15:07:49 +0800

    吕龙江搭建VUE项目框架上传VUE

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+//配置axios文件
+import axios from 'axios'
+//配置element-plus文件
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
+//使用axios
+const app=createApp(App)
+app.config.globalProperties.$axios=axios
+app.use(store).use(router).use(ElementPlus).mount('#app')
